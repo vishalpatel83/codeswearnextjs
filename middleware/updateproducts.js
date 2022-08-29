5eaceabbd2863cb03097321dbc87bce896642c7b
DESKTOP-H9SO2E8\Shree <pv834763gmail.com>
2022-08-29 11:53:13 -0700

    update product api

---
+++ middleware/updateproducts.js
@@ -0,0 +1,16 @@
+import Product from "../models/Products";
+import connectDb from "./mongoose";
+
+const handler=async (req,res)=>{
+    if(req.method=== 'post'){
+    for(let i=0;i<req.body.length;i++){
+   let p=await Product.findByIdAndUpdate(req.body[i]._id,req.body[i])
+}
+res.status(200).json({success:"success"})
+}
+else{
+    res.status(400).json({error:"this method is not allowed"})
+}
+ }
+
+ export default connectDb(handler);
